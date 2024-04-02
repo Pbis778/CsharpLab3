578f61013c8c351c145b0d4e7922f73d8617048f
Pbis778 <pb305347@student.polsl.pl>
2024-04-02 12:37:54 +0200

    lab3_zadania

---
+++ Lab3Csharp/Motoryzacja/Pojazd.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Motoryzacja
+{
+    struct RejestrNazw
+    {
+        public DateTime DataModyfikacji { get; set; }
+        public string Nazwa { get; set; }
+
+        public RejestrNazw(DateTime dataModyfikacji, string nazwa)
+        {
+            DataModyfikacji = dataModyfikacji;
+            Nazwa = nazwa;
+        }
+
+        public override string ToString()
+        {
+            return $"{DataModyfikacji}: {Nazwa}";
+        }
+    }
+
+    public class Pojazd
+    {
+        public string Nazwa { get; set; }
+        private int liczbaKol;
+        private double predkosc;
+        public int Lp { get; }
+
+        private static int liczbaPojazdow = 0;
+        private List<RejestrNazw> HistoriaNazw { get; set; }
+
+        public Pojazd()
+        {
+            Lp = liczbaPojazdow++;
+            HistoriaNazw = new List<RejestrNazw>();
+        }
+
+        public int LiczbaKol
+        {
+            get { return this.liczbaKol; }
+            set
+            {
+                if (value < 2)
+                {
+                    throw new ArgumentException();
+                }
+                else { this.liczbaKol = value; };
+            }
+        }
+
+        public double Predkosc
+        {
+            get { return predkosc; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else { this.predkosc = value; };
+            }
+        }
+
+        public Pojazd(string nazwa, int liczbaKol, double predkosc) : this()
+        {
+            Nazwa = nazwa;
+            LiczbaKol = liczbaKol;
+            Predkosc = predkosc;
+            DodajDoHistorii();
+        }
+
+        public Pojazd(string nazwa, double predkosc) : this(nazwa, 4, predkosc) { }
+
+        public void DodajDoHistorii()
+        {
+            HistoriaNazw.Add(new RejestrNazw(DateTime.Now, Nazwa));
+        }
+
+        public List<string> PobierzHistorie()
+        {
+            List<string> historia = new List<string>();
+            foreach (var rejestrNazw in HistoriaNazw)
+            {
+                historia.Add(rejestrNazw.ToString());
+            }
+            return historia;
+        }
+
+        public override string ToString()
+        {
+            return $"Liczba pojazdów: {Lp + 1}/{liczbaPojazdow}, Nazwa: {Nazwa}, Liczba kół: {liczbaKol}, Prędkość: {predkosc}";
+        }
+    }
+
+    public class PojazdMechaniczny : Pojazd
+    {
+        public double MocSilnika { get; set; }
+
+        public PojazdMechaniczny(string nazwa, int liczbaKol, double predkosc, double mocSilnika) : base(nazwa, liczbaKol, predkosc)
+        {
+            MocSilnika = mocSilnika;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Moc silnika: {MocSilnika}";
+        }
+    }
+
+    public class Samochod : PojazdMechaniczny
+    {
+        public int LiczbaPasazerow { get; set; }
+        public string Marka { get; set; }
+
+        public Samochod(string nazwa, int liczbaKol, double predkosc, double mocSilnika, int liczbaPasazerow, string marka) : base(nazwa, liczbaKol, predkosc, mocSilnika)
+        {
+            LiczbaPasazerow = liczbaPasazerow;
+            Marka = marka;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Liczba pasazerow: {LiczbaPasazerow}, Marka samochodu: {Marka}";
+        }
+    }
+}
---
+++ Lab3Csharp/lab3/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Motoryzacja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZadA_Click(object sender, RoutedEventArgs e)
+        {
+            List<Pojazd> pojazdy = new List<Pojazd>
+            {
+                new Pojazd("Samochód", 4, 120),
+                new Pojazd("Motocykl", 2, 180),
+                new Pojazd("Rower", 2, 30),
+                new Pojazd("Autobus", 6, 80),
+                new PojazdMechaniczny("Ciągnik", 4, 50, 100),
+                new Samochod("Sedan", 4, 150, 200, 5, "Toyota")
+            };
+
+            pojazdy[0].DodajDoHistorii();
+            WyswietlHistorie(pojazdy[0]);
+            WyswietlPojazdy(pojazdy);
+        }
+
+        private void WyswietlPojazdy(List<Pojazd> pojazdy)
+        {
+            lbxZadA.Items.Clear();
+
+            foreach (Pojazd pojazd in pojazdy)
+            {
+                lbxZadA.Items.Add(pojazd.ToString());
+            }
+        }
+
+        private void WyswietlHistorie(Pojazd pojazd)
+        {
+            HistoriaListBox.Items.Clear();
+
+            foreach (var wpis in pojazd.PobierzHistorie())
+            {
+                HistoriaListBox.Items.Add(wpis);
+            }
+        }
+    }
+}
